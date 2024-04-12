44632178aa2eefb34b50b1013e3ba36b85da3717
meichiii2003 <meichiii2003@gmail.com>
2024-04-12 20:50:20 +0800

    merge complete

--- frontend/src/app/job-opening/page.tsx
+++ frontend/src/app/job-opening/page.tsx
@@ -6,11 +6,7 @@ import IndiJobCategory, { JobCatType} from './indiJobCategory';
 
 async function getData() {
   // sample data
-<<<<<<< HEAD
-  const res = await fetch('https://4x174vzx-8000.asse.devtunnels.ms/jobopenings', { cache: "no-store" });
-=======
   const res = await fetch('http://localhost:8000/jobopenings', { cache: "no-store" });
->>>>>>> main
   const data = await res.json();
   console.log("request data done");
   return data;
