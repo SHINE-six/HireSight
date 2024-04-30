90c332d3f4ae08083c068ef73570e073d9ab7f92
Puah Yi Kai <puahyikai@gmail.com>
2024-04-30 23:33:18 +0800

    added on interviewee report, left with button click

---
+++ frontend-hr/src/app/interviewee-report/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import View from "./PDFView";
+
+// const Template = dynamic(() => import("./PDFFileTSX"), {
+//   loading: () => <p>Loading...</p>,
+//   ssr: false,
+//   });
+
+const MyPage = () => {
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-0">
+      <View />
+    {/* <MyRadarChart /> */}
+    </main>
+  );
+};
+
+export default MyPage;
