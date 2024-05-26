c8d27f610c5541ae42961e462ad1272db515f8d1
anning0104 <anning7927@gmail.com>
2024-05-26 19:50:34 +0800

    stupid gemini generate report

--- backend/behavioralAnalysis.py
+++ backend/behavioralAnalysis.py
@@ -1,25 +1,22 @@
-
-
 def main(combinedJsonData):
-    emotion = combinedJsonData["sections"][0]["emotion"]
+    emotion = combinedJsonData["emotion"]
     anger = emotion.get("Angry", 0)
     disgust = emotion.get("Disgust", 0)
     fear = emotion.get("Fear", 0)
     sad = emotion.get("Sad", 0)
     emotion_prob = weights['anger'] * anger + weights['disgust'] * disgust + weights['fear'] * fear + weights['sad'] * sad
     
-    eye = combinedJsonData["sections"][0]["eye"]
+    eye = combinedJsonData["eye"]
     blink = eye.get("blink episode", 0)
     gaze_right = eye['gaze'].get("RIGHT", 0)
     gaze_left = eye['gaze'].get("LEFT", 0)
     eye_prob = weights['blink'] * blink + weights['gaze'] * (gaze_right + gaze_left)
 
-    result = weights['alpha'] * emotion_prob + weights['beta'] * eye_prob 
+
     formatted_result = "{:.2f}".format(result)
     result = {
         "Score": f"{formatted_result}%",
     }
-
     return result
 
 weights = {
@@ -31,4 +28,4 @@ weights = {
     'gaze': 0.4,
     'alpha': 0.3,
     'beta': 0.4,
-    }
+}
--- backend/testingllmreport.py
+++ backend/testingllmreport.py
@@ -19,11 +19,12 @@ def concat_all_transcript():
             log_full += log_format
     return log_full 
 
-mbti_type = "intj" 
+mbti_type = "intj" #get from reportData
 concatTranscript = concat_all_transcript()
 interviewDate = datetime.today().strftime('%B %d, %Y')
+
 toStoreJson = {
-    "InterveweeName": "Desdesdes",
+    "InterveweeEmail": "Desdesdes",
     "InterveweeID": "122ed2",
     "OverallSuitability": 90,
     "InterviewPosition": "IT BUSINESS ANALYST (SUSTAINABILITY)- Fresh Graduates",
@@ -31,17 +32,15 @@ toStoreJson = {
     "RadarChartSummary": "The candidate demonstrated strong technical expertise in ERP systems and toStoreJson analysis, particularly with tools like SAP S/4HANA, Power BI, and SQL. They were exceptionally prepared, showing a thorough understanding of our company's challenges and the wider industry. Their effective communication and collaborative skills, coupled with their adaptability and professionalism, make them a standout candidate and a potential asset to our team.",
     "ai_report": None
 }
-
+# put chenming formal and tidiness combinedData and get it to main()
 try:
     ai_report = None
     if ai_report == None:
         # Vertex Ai    
-        ai_report = LLM_report_2.main(concatTranscript, mbti_type)
+        ai_report = LLM_report_2.main(concatTranscript, mbti_type) 
         # Genmini Ai
         # ai_report = LLM_report.main(concatTranscript, mbti_type)
-
     toStoreJson["ai_report"] = ai_report
-
 except Exception as e:
     print(f"An error occurred: {e}")
     print("Retrying in 5 seconds...")
